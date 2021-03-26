31ae8e7142c250ed972c2a99c23e3680295dd3a5
Adrian Camacho <adriancamacho18@gmail.com>
2021-03-26 11:29:41 -0700

    add initial language to scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-  
+  $("#survey").submit(function(event) {
+    event.preventDefault();
+    const age = parseInt($("#age").val());
+    const food = $("#food").val();
+    const music = $("#music").val();
+    const vacation = $("#vacation").val();
+    const beverage = $("#beverage").val();
+  });
 });
