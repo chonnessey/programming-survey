d0f10c1477731c6b2332bf9ce46c120c53cc7b6d
Adrian Camacho <adriancamacho18@gmail.com>
2021-03-26 13:28:18 -0700

    add remainingif functions to python and c classes

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,15 @@ $(document).ready(function() {
     const vacation = $("#vacation").val();
     const beverage = $("#beverage").val();
 
-    if (age > 12 && food === "tacos") {
+    if (age > 1 && food === "tacos") {
       $(".java").show();
-    } else if (age > 1 && food === "pizza" && music === "jazz") {
+    } else if (age > 1 && food === "pancakes" && music === "jazz") {
+      $(".python").show();
+    } else if (age > 1 && beverage === "beer" && vacation === "mexico") {
+      $(".c").show();
+    } else if (age > 1 && food === "rice") {
+      $(".c").show();
+    } else {
       $(".python").show();
     }
 
